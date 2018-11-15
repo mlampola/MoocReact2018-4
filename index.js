3709309bd7cbe378eca0794304297ea275030360
Markus Lampola <markus.lampola@opr-vakuus.fi>
2018-11-15 23:43:52 +0200

    4.2 blogilista, osa 2

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -29,25 +21,17 @@ const MONGO_PASS = process.env.MONGO_PASS
 const MONGO_DB = process.env.MONGO_DB
 
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_DB}`
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true })
+  .then( () => {
+    console.log('connected to database', MONGO_DB)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
